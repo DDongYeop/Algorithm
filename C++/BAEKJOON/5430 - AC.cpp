7f4f5c4e47657fa891eb5ac09f1a66fd32ae0cef
DDongYeop <kitty-777@naver.com>
2023-05-17 17:31:52 +0900

    C++ - 백준 - 5430

--- C++/BAEKJOON/5430 - AC.cpp
+++ C++/BAEKJOON/5430 - AC.cpp
@@ -2,27 +2,45 @@
 #include<deque>
 using namespace std;
 
-void DequeFlip(deque<int>& deq);
-
 int main()
 {
     string str, str2;
-    int input, input2;
-    bool isError = false;
+    int input, input2, strIndex = 1, pushIndex = 0;
+    bool isError = false, isReverse = false;
     cin >> input;
 
     for (int i = 0; i < input; ++i)
     {
-        isError = false;
+        isError = isReverse = false;
         deque<int> deq;
         cin >> str >> input2 >> str2;
-        for (int j = 0; j < input2; ++j)
-            deq.push_back((int)str2[1 + 2 * j] - '0');
+        pushIndex = 0;
+        strIndex = 1;
+        while (true)
+        {
+            if (str2[strIndex] == ',')
+            {
+                ++strIndex;
+                deq.push_back(pushIndex);
+                pushIndex = 0; 
+                continue;
+            }
+            else if (str2[strIndex] == ']')
+            {
+                if (pushIndex != 0)
+                    deq.push_back(pushIndex);
+                break;
+            }
+
+            pushIndex *= 10;
+            pushIndex += (int)str2[strIndex] - '0';
+            ++strIndex;
+        }
 
         for (int j = 0; j < str.size(); ++j)
         {
             if (str[j] == 'R')
-                DequeFlip(deq);
+                isReverse = !isReverse;
             else
             {
                 if (deq.empty())
@@ -30,13 +48,18 @@ int main()
                     isError = true;
                     break;
                 }
-                deq.pop_front();
+                if (!isReverse)
+                    deq.pop_front();
+                else
+                    deq.pop_back();
             }
         }
 
-        if (isError || deq.empty())
+        if (isError)
             cout << "error\n";
-        else
+        else if (deq.empty())
+            cout << "[]\n";
+        else if (!isReverse)
         {
             cout << '[' << deq.front();
             deq.pop_front();
@@ -47,17 +70,16 @@ int main()
             }
             cout << "]\n";
         }
+        else
+        {
+            cout << '[' << deq.back();
+            deq.pop_back();
+            while (!deq.empty())
+            {
+                cout << ',' << deq.back();
+                deq.pop_back();
+            }
+            cout << "]\n";
+        }
     }
-}
-
-void DequeFlip(deque<int>& deq)
-{
-    int index = deq.size();
-    deque<int> deq2;
-    for (int i = 0; i < index; ++i)
-    {
-        deq2.push_front(deq.front());
-        deq.pop_front();
-    }
-    deq = deq2;
 }
