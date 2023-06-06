40883947d5b4e08ce2b01f2c42053e8186ee90c2
DDongYeop <kitty-777@naver.com>
2023-06-07 00:35:50 +0900

    C++ - 백준 - 9655, 9656

---
+++ C++/BAEKJOON/9655 - 돌 게임.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    cin >> input;
+
+    if (input % 2 == 0)
+        cout << "CY";
+    else 
+        cout << "SK";
+
+    return 0;
+}
---
+++ C++/BAEKJOON/9656 - 돌 게임 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    cin >> input;
+
+    if (input % 2 == 1)
+        cout << "CY";
+    else 
+        cout << "SK";
+
+    return 0;
+}
