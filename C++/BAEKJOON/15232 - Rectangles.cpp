b2028330133fd770c0687568916383e450cf1eb4
DDongYeop <kitty-777@naver.com>
2023-06-12 16:48:22 +0900

    C++ - 백준 - 15232

--- C++/BAEKJOON/15232 - Rectangles.cpp
+++ C++/BAEKJOON/15232 - Rectangles.cpp
@@ -8,7 +8,7 @@ int main()
     cin >> x >> y;
     for (int i = 0; i < x; ++i)
     {
-        for (int j = 0; i < y; ++j)
+        for (int j = 0; j < y; ++j)
             cout << '*';
         cout << '\n'; 
     }
