9d149cf8cf4381a459f4c6855b1a1ae03a526a25
DDongYeop <ddongyeop0129@gmail.com>
2023-07-08 22:18:58 +0900

    C++ - 백준 - 3046

---
+++ C++/BAEKJOON/3046 - R2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char* argv[])
+{
+	int n, avg,result;
+	cin >> n >> avg;
+	result = (avg * 2) - n;
+	cout << result << endl;
+	return 0;
+}
+
