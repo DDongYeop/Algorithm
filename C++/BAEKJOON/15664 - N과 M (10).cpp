85e9edcd27c9b9d95320a1715bdb748005703027
DDongYeop <ddongyeop0129@gmail.com>
2023-08-03 18:04:49 +0900

    C++ - 백준 - 15663 ~ 15666

---
+++ C++/BAEKJOON/15664 - N과 M (10).cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[9];
+vector<int> printVec;
+bool visited[9];
+
+void BackTracking(int n, int m, int num, int cnt);
+
+int main()
+{
+    int n, m, input;
+    cin >> n >> m;
+    printVec.push_back(-2147483647);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> input;
+        printVec.push_back(input);
+    }
+    sort(printVec.begin(), printVec.end());
+    BackTracking(n, m, 1, 0);
+}
+
+void BackTracking(int n, int m, int num, int cnt)
+{
+    int last = 0;
+
+    if (cnt == m)
+    {
+        for (int i = 0; i < cnt; ++i)
+            cout << printVec[arr[i]] << ' ';
+        cout << '\n';
+    }
+    else
+    {
+        for (int i = num; i <= n; ++i)
+        {
+            if (visited[i] || last == printVec[i])
+                continue;
+            arr[cnt] = i;
+            visited[i] = true;
+            last = printVec[i];
+            BackTracking(n, m, i + 1, cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
