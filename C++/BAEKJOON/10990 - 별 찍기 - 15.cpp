abf9fca98b29d1cda01a441cb6dfed31361e8b49
DDongYeop <kitty-777@naver.com>
2023-08-29 21:40:29 +0900

    C++ - 백준 - 10990

---
+++ C++/BAEKJOON/10990 - 별 찍기 - 15.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if (num == 1)
+    {
+        cout << "*" << '\n';
+        return 0;
+    }
+    for (int i = 1; i <= num - 1; i++)
+        cout << " ";
+
+    cout << "*" << '\n';
+    for (int i = 1; i < num; i++)
+    {
+        for (int t = 1; t <= num - (i+1); t++)
+            cout << " ";
+
+        cout << "*";
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << " ";
+
+        cout << "*" << '\n';
+    }
+}
