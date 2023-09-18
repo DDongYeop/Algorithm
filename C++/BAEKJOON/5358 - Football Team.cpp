88e7c17e380fe56c8b53f1826e83432c94e4ee71
DDongyeop <ddongyeop0129@gmail.com>
2023-09-18 22:28:57 +0900

    C++ - 백준 - 5358

---
+++ C++/BAEKJOON/5358 - Football Team.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    while (1)
+    {
+        getline(cin, s);
+        if (cin.eof())
+            break;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'e')
+                cout << 'i';
+            else if (s[i] == 'i')
+                cout << 'e';
+            else if (s[i] == 'E')
+                cout << 'I';
+            else if (s[i] == 'I')
+                cout << 'E';
+            else
+                cout << s[i];
+        }
+        cout << "\n";
+    }
+}
