eea74fe0089082fd0485476c928c2faecb3dc975
DDongYeop <ddongyeop0129@gmail.com>
2023-10-01 21:18:46 +0900

    C++ - 백준 - 11365

---
+++ C++/BAEKJOON/11365 - !밀비 급일.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    while (getline(cin, str))
+    {
+        if (str == "END")
+            break;
+        for (int i = str.size()-1; i >= 0; --i)
+            cout << str[i];
+        cout << '\n';
+    }
+}
