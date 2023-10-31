8fbd3a07fa59dd7000d61dee8623112cd89957fc
Dongyeop <ddongyeop0129@gmail.com>
2023-10-31 23:10:55 +0900

    C++ - 백준 - 20254

---
+++ C++/BAEKJOON/20254 - Site Score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input1, input2, input3, input4, answer;
+    cin >> input1 >> input2 >> input3 >> input4;
+    cout << (input1 * 56) + (input2 * 24) + (input3 * 14) + (input4 * 6); 
+}
