efcf4125ded4df0911ca53a2be23c792426b238d
Dongyeop <ddongyeop0129@gmail.com>
2023-12-05 23:14:04 +0900

    C++ - 백준 - 11948

---
+++ C++/BAEKJOON/11948 - 과목선택.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() 
+{
+    int s[4],m[2];
+    for (int i = 0; i < 4; i++)
+        cin >> s[i];
+    sort(s, s + 4);
+    for (int i = 0; i < 2; i++)
+        cin >> m[i];
+    sort(m, m + 2);
+    
+    cout << s[1] + s[2] + s[3] + m[1]<<'\n';
+}
