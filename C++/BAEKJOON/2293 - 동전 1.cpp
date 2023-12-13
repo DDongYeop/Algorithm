8f570e4c6982038d2556781956571875065d0b20
Dongyeop <ddongyeop0129@gmail.com>
2023-12-13 21:44:09 +0900

    C++ - 백준 - 2293

---
+++ C++/BAEKJOON/2293 - 동전 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int coin[101];
+    int dp[10001]={1};
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+        cin >> coin[i];
+
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = coin[i]; j <= k; j++)
+                dp[j] += dp[j - coin[i]];
+    }
+
+    cout << dp[k];
+}
